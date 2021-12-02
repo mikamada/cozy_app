70e04b5b03c39b6c4c7a28266644ec8cedc10838
mikamada <mikamada17@gmail.com>
2021-12-02 07:08:34 +0700

    slicing ui splash_page.dart

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,79 @@
+import 'package:cozy_app/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                'assets/bottom.png',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, top: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 30),
+                    width: 50,
+                    height: 50,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/logo.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Find Cozy House\nto Stay and Happy',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 40),
+                    width: 210,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: purpleColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Get Started',
+                        style: whiteTextStyle.copyWith(
+                            fontSize: 18, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
